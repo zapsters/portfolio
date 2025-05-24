5c52ee5409bf0ed6832ccae3ee090c941ab1f4cd
zapsters <erinballinger24@gmail.com>
2025-05-24 17:38:18 -0400

    Now building in the back end. + Changed the way webpack compiles portfolio to be a more conventional method.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,39 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  devtool: "eval-source-map",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist/app"),
-    filename: "app.js",
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
-  watch: true,
+  mode: "development", // use 'production' for builds
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader", // Injects styles into DOM
+          "css-loader", // Turns CSS into JS
+          "sass-loader", // Compiles Sass to CSS
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    port: 3000,
+    open: true,
+    hot: true,
+  },
 };
