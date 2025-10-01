753d7bb92bca1bc4e79966bbe54d600c855fe0d9
zapsters <erinballinger24@gmail.com>
2025-09-30 21:31:31 -0400

    Only allow dark mode at the moment... Will re-enable once I finish styling the light mode.

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider attribute="data-bs-theme" enableSystem>
+        <ThemeProvider attribute="data-bs-theme" defaultTheme="dark">
           <Marquee autoFill={true} speed={50} className="marquee">
             <span className="bianzhidai">{marqueeSpacer()}❤ FULL STACK WEB DEVELOPER ❤</span>
             <span className="bianzhidai">
