29bb547a0cd8e33c1cb70aaf1d472fca7ec168e2
zapsters <erinballinger24@gmail.com>
2025-09-30 20:20:38 -0400

    Eslint escaped quotes

--- app/projects/BLIMP/page.tsx
+++ app/projects/BLIMP/page.tsx
@@ -3,8 +3,8 @@ export default function Projects() {
     <>
       <h1>B.L.I.M.P.</h1>
       <p>
-        The Balloon Live Instrumentation Monitoring Platform ("BLIMP"), is Lorem ipsum dolor sit,
-        amet consectetur adipisicing elit. Corporis laudantium officiis vitae temporibus
+        The Balloon Live Instrumentation Monitoring Platform (&quot;BLIMP&quot;), is Lorem ipsum
+        dolor sit, amet consectetur adipisicing elit. Corporis laudantium officiis vitae temporibus
         voluptatibus earum illum delectus. Voluptatibus eligendi possimus ipsum adipisci pariatur
         tempora atque? Ut tenetur accusantium quas facere.
       </p>
