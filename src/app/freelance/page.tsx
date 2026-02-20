5ae7d67ab21d9cbadb9002663997cadbfd8aeb63
zapsters <erinballinger24@gmail.com>
2026-02-20 12:45:03 -0500

    Added freelance page

---
+++ src/app/freelance/page.tsx
@@ -0,0 +1,11 @@
+import Github from "../components/github";
+import Linkedin from "../components/linkedin";
+
+export default function Contact() {
+  return (
+    <>
+      <h1>Freelance</h1>
+      <p>hi!</p>
+    </>
+  );
+}
