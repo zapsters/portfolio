9468c6b824152491c0d01e3995267c00223b34fa
zapsters <erinballinger24@gmail.com>
2026-02-20 12:44:38 -0500

    Added wrapper style prop to Pixelated component

--- src/app/components/pixelated.tsx
+++ src/app/components/pixelated.tsx
@@ -1,12 +1,13 @@
 import { CSSProperties } from "react";
 
 type PixelatedProp = React.PropsWithChildren<{
+  wrapperStyle?: CSSProperties | undefined;
   style?: CSSProperties | undefined;
 }>;
 
-export default function Pixelated({ children, style }: PixelatedProp) {
+export default function Pixelated({ children, wrapperStyle, style }: PixelatedProp) {
   return (
-    <div className="pixel-corners--wrapper">
+    <div className="pixel-corners--wrapper" style={wrapperStyle}>
       <div className="pixel-corners" style={style}>
         {children}
       </div>
